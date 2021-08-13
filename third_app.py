a84cfea4e1d527765bf13a4e8bb8ba9956394866
JanKluewer <87062222+JanKluewer@users.noreply.github.com>
2021-08-13 08:59:52 +0200

    created third app to implement for loop in the map layers

---
+++ third_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pydeck as pdk
+from urllib.error import URLError
+import pandas as pd
+
+
+dfcsv= pd.read_csv('C:/Users/jklue/OneDrive/Desktop/output_data55.csv')
+amenities = ["school","hospital"]
+def map():
+    for x in amenities:
+        try:
+            ALL_LAYERS = {
+                x: pdk.Layer(
+                    "ScatterplotLayer",
+                    data=dfcsv[dfcsv["amenity"] == x],
+                    get_position=["longitude", "latitude"],
+                    get_color=[200, 30, 0, 160],
+                    get_radius=1000,
+                    radius_scale=0.05,
+                ),
+            }
+            st.sidebar.markdown('### Map Layers')
+            selected_layers = [
+                layer for layer_name, layer in ALL_LAYERS.items()
+                if st.sidebar.checkbox(layer_name, True)]
+            if selected_layers:
+                st.pydeck_chart(pdk.Deck(
+                   map_style="mapbox://styles/mapbox/light-v9",
+                   initial_view_state={"latitude": 51.24,
+                                       "longitude": 6.85, "zoom": 11, "pitch": 50},
+                   layers=selected_layers,
+               ))
+            else:
+                st.pydeck_chart(pdk.Deck(
+                    map_style="mapbox://styles/mapbox/light-v9",
+                    initial_view_state={"latitude": 51.24,
+                                        "longitude": 6.85, "zoom": 11, "pitch": 50}))
+        except URLError as e:
+            st.error("""Connection error: %s""" % e.reason)
+
+st.write("15-Minute-City-all in one dataframe")
+map()
+
+
