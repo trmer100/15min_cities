8bf7bf7bcd1a78119e4e1dbeea470f90fc857992
Jan Klüwer <jkluewer@gmx.de>
2021-08-30 14:48:38 +0200

    ++

--- ausprobieren.py
+++ ausprobieren.py
@@ -1,13 +1,6 @@
-import pandas as pd
 from grid2 import cells  # this will transfer the output from grid2 to this script
-dfcells = pd.DataFrame.from_dict(cells)
-print(dfcells)
-
-
-
 from scipy import spatial
+listcells = list(cells.keys())
 point = (51.2290062, 6.8212808624507195)
-airports = [(50.22,6.77),(51.33,8.77),(49.2256,6.88897),(55.1123,6.11231)]
-tree = spatial.KDTree(airports)
-print(tree.query([(49.2290062,6.8212808624507195)]))
-#(array([ 1.41421356]), array([1]))
+tree = spatial.KDTree(listcells)
+print(tree.query([point]))
