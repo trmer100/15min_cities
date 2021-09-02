f468f1ce73de7a4a34a2fd995679447080328d88
Jan Klüwer <jkluewer@gmx.de>
2021-09-02 16:54:36 +0200

    deleted test files

--- ausprobieren.py
+++
@@ -1,6 +0,0 @@
-from grid2 import cells  # this will transfer the output from grid2 to this script
-from scipy import spatial
-listcells = list(cells.keys())
-point = (51.2290062, 6.8212808624507195)
-tree = spatial.KDTree(listcells)
-print(tree.query([point]))
