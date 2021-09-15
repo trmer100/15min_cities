52e29fc4908444f035d2d878d22470f154b2c786
Jan Klüwer <jkluewer@gmx.de>
2021-09-15 12:11:32 +0200

    new version but with errors

--- 15minapp.py
+++ 15minapp.py
@@ -3,9 +3,14 @@ import pydeck as pdk
 from geopy.geocoders import Nominatim
 import pandas as pd
 import numpy as np
+import sys
 
-# Creates a grid. Grid points consist out of longitude and latitude information. Distance between grid points = d. Amenities within a radius (r) of each grid point are assigned to each grid point and saved in a dictionary.
+
 def precompute_grid(csv_path, begin_lat, end_lat, begin_long, end_long, d, r):
+    """
+    Creates a grid. Grid points consist out of longitude and latitude information.
+    Distance between grid points = d. Amenities within a radius (r) of each grid point are assigned to each grid point and saved in a dictionary.
+    """
     places = pd.read_csv(csv_path)
 
     places["latitude"] = places["latitude"].astype(float)
@@ -44,48 +49,9 @@ def precompute_grid(csv_path, begin_lat, end_lat, begin_long, end_long, d, r):
                         break
     return cells
 
-#Lat1 = 51.1238
-#Lat2 = 51.3539
-#Long1 = 6.6824
-#Long2 = 6.94
-
-#d = 0.001
-#r = 0.01  # should be 1km
-
-#connection to openstreetmaps_query.py read in of amenities_df.csv (cointains all amenities). 
-#file_path = 'amenities_df.csv'
-#cells = precompute_grid(file_path, Lat1, Lat2, Long1, Long2, d, r)
-#cells_df = pd.DataFrame.from_dict(cells, orient = "index")
-#pd.DataFrame.reset_index(cells_df, inplace = True)
-#pd.DataFrame.rename(cells_df,columns={'level_0': 'latitude', 'level_1': 'longitude'}
-#,inplace = True)
-#cells_df.fillna(value = 0,inplace = True)
-#cells_df.to_csv("cells_df.csv")
-
-
-st.title("15-Minute-City")
-st.write("Please insert your address and provide your preferences on the left. When finished, please click the """"Create Map""" "button to show how good your address and the surrounding area fits the 15-minute city approach!")
-all_amenities_df = pd.read_csv("amenities_df.csv") 
-
-
-individual_values = all_amenities_df[
-    "amenity"].unique()  
-amenities = []  
-layer = [] 
-slider_values = []
-
-
-for x in individual_values:
-    checkbox = st.sidebar.checkbox(x, True)
-    slider_text = "Please choose your weight for " + x
-    slider_value = st.sidebar.slider(slider_text, min_value=1, max_value=10, step=1)
-    if checkbox == True:
-        amenities.append(x)  # append x so the system knows which amenities to display on the map
-        slider_values.append(slider_value)
-
 
 def map(amenities):
-    heat_layer = pdk.Layer(  # https://deck.gl/docs/api-reference/aggregation-layers/heatmap-layer
+    heat_layer = pdk.Layer(
         "HeatmapLayer",
         cells_df,
         radiusPixels=125,
@@ -93,7 +59,8 @@ def map(amenities):
         get_position=["longitude", "latitude"],
         threshold=0.1,
         get_weight="total_score",
-        pickable=True, )
+        pickable=True,
+    )
     layer.append(heat_layer)
 
     for amenity_layer in amenities:
@@ -103,7 +70,8 @@ def map(amenities):
             get_position=["longitude", "latitude"],
             get_color=[0, 0, 0, 1000],
             get_radius=200,
-            radius_scale=0.05, )
+            radius_scale=0.05,
+        )
         layer.append(amenity_layer)
     user_home = pdk.Layer(
         "ScatterplotLayer",
@@ -111,50 +79,122 @@ def map(amenities):
         get_position=["longitude", "latitude"],
         get_color=[185, 207, 234, 150],
         get_radius=600,
-        radius_scale=1.05,)
+        radius_scale=1.05,
+    )
     layer.append(user_home)
 
-    st.pydeck_chart(pdk.Deck(map_style="mapbox://styles/mapbox/light-v9",
-                             initial_view_state={"latitude": 51.24, "longitude": 6.85, "zoom": 11, "pitch": 50},
-                             layers=layer))
+    st.pydeck_chart(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": 51.24,
+                "longitude": 6.85,
+                "zoom": 11,
+                "pitch": 50,
+            },
+            layers=layer,
+        )
+    )
     return
 
 
 def address():
     geolocator = Nominatim(
-        user_agent="my_user_agent")  # this and the line below will return the coordinates after providing an address in a certain format
+        user_agent="my_user_agent"
+    )  # this and the line below will return the coordinates after providing an address in a certain format
     loc = geolocator.geocode(full_address)
     # st.write("latitude:" ,loc.latitude,"\nlongtitude:" ,loc.longitude)
-    return pd.DataFrame({"amenity": ["user_home"], "latitude": [loc.latitude], "longitude": [loc.longitude]})
+    return pd.DataFrame(
+        {
+            "amenity": ["user_home"],
+            "latitude": [loc.latitude],
+            "longitude": [loc.longitude],
+        }
+    )
 
 
-user_street = st.text_input("Street",
-                            "Königsallee")  # st.text_input returns the input of the user to a variable, in this case user_street, "Königsallee" is the default value here
-user_street_number = st.text_input("House number", "1")
-user_city = st.text_input("City", "Düsseldorf")
-user_country = "DE"
-full_address = str(user_street) + " " + str(user_street_number) + "," + str(user_city) + "," + user_country
+if __name__ == "__main__":
 
-df1 = pd.DataFrame(address())  # assigning the address to df1 in order to use it in the function map()
-user_address = address()
+    if len(sys.argv) > 1 and sys.argv[1] == "run_grid":
+        Lat1 = 51.1238
+        Lat2 = 51.3539
+        Long1 = 6.6824
+        Long2 = 6.94
+        d = 0.001
+        r = 0.01  # should be 1km
 
-if st.button("Create Map"):
-    cells_df = pd.read_csv("cells_df.csv")
-    amenities_df = pd.DataFrame(amenities)
-    slider_values_df = pd.DataFrame(slider_values)
-    amenities_df.insert(1, "weight", slider_values_df, True)
-    #amenities_df.to_csv("amenities_weights.csv")
-    #user_address.to_csv("user_address.csv")
-    cells_df["total_score"] = 0
-    for amenity in individual_values:
-        weight_df = amenities_df[amenities_df[0] == amenity]
-        weight_raw = weight_df["weight"].values
-        if weight_raw > 0:
-            weight = weight_raw
-        else:
-            weight = 0
-        weight = cells_df[amenity].astype(int) * weight
-        cells_df["total_score"] = cells_df["total_score"] + weight
-    map(amenities)
+        # connection to openstreetmaps_query.py read in of amenities_df.csv (cointains all amenities).
+        file_path = "amenities_df.csv"
+        cells = precompute_grid(file_path, Lat1, Lat2, Long1, Long2, d, r)
+        cells_df = pd.DataFrame.from_dict(cells, orient="index")
+        pd.DataFrame.reset_index(cells_df, inplace=True)
+        pd.DataFrame.rename(
+            cells_df,
+            columns={"level_0": "latitude", "level_1": "longitude"},
+            inplace=True,
+        )
+        cells_df.fillna(value=0, inplace=True)
+        cells_df.to_csv("cells_df.csv")
 
+    st.title("15-Minute-City")
+    st.write(
+        "Please insert your address and provide your preferences on the left. When finished, please click the "
+        """Create Map"""
+        "button to show how good your address and the surrounding area fits the 15-minute city approach!"
+    )
+    all_amenities_df = pd.read_csv("amenities_df.csv")
 
+    individual_values = all_amenities_df["amenity"].unique()
+    amenities = []
+    layer = []
+    slider_values = []
+
+    for individual_value in individual_values:
+        checkbox = st.sidebar.checkbox(individual_value, True)
+        slider_text = "Please choose your weight for " + individual_value
+        slider_value = st.sidebar.slider(slider_text, min_value=1, max_value=10, step=1)
+        if checkbox == True:
+            amenities.append(individual_value)
+            slider_values.append(slider_value)
+
+    user_street = st.text_input(
+        "Street", "Königsallee"
+    )  # st.text_input returns the input of the user to a variable, in this case user_street, "Königsallee" is the default value here
+    user_street_number = st.text_input("House number", "1")
+    user_city = st.text_input("City", "Düsseldorf")
+    user_country = "DE"
+    full_address = (
+        str(user_street)
+        + " "
+        + str(user_street_number)
+        + ","
+        + str(user_city)
+        + ","
+        + user_country
+    )
+
+    df1 = pd.DataFrame(
+        address()
+    )  # assigning the address to df1 in order to use it in the function map()
+    user_address = address()
+
+    if st.button("Create Map"):
+        cells_df = pd.read_csv("cells_df.csv")
+        amenities_df = pd.DataFrame(amenities)
+        slider_values_df = pd.DataFrame(slider_values)
+        amenities_df.insert(1, "weight", slider_values_df, True)
+        # amenities_df.to_csv("amenities_weights.csv")
+        # user_address.to_csv("user_address.csv")
+        cells_df["total_score"] = 0
+        for amenity in individual_values:
+            weight_df = amenities_df[amenities_df[0] == amenity]
+            weight_raw = weight_df["weight"].values
+            if weight_raw > 0:
+                weight = weight_raw
+            else:
+                weight = 0
+            weight = cells_df[amenity].astype(int) * weight
+            cells_df["total_score"] = cells_df["total_score"] + weight
+        map(amenities)
+
+    # remove files. gitignore
